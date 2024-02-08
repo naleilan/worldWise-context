f437683de81d912ccdb342f2c669557fb85bbe6f
naleilan <leilan.n@gmail.com>
2024-02-08 10:06:14 +0300

    add context to component

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,9 +1,12 @@
+import { useCities } from "../contexts/CitiesContext";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
 import Spinner from "./Spinner";
 import styles from "./countryList.module.css";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { country, cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
